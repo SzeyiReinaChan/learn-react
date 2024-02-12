147c07cc609d0c25705b065e1199621133c9b72a
szeyic@gmail.com <szeyic@gmail.com>
2024-02-12 16:20:49 -0500

    class activity

--- src/pages/qcomps/list_keys_id.js
+++ src/pages/qcomps/list_keys_id.js
@@ -13,11 +13,12 @@ export const people = [{
 }];
 
 function getImageUrl(imageId) {
-    return "https://i.imgur.com/" + imageId + "s.jpg"
+  return "https://i.imgur.com/" + imageId + "s.jpg"
 }
 export default function List() {
   const listItems = people.map(person =>
-    <li>
+    <li key={person.imageId}>
+      <img src={getImageUrl(person.imageId)}></img>
     </li>
   );
   return <ul>{listItems}</ul>;
