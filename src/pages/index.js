66701db93e94fce1ca63d12d1a00482b3ba6427e
szeyic@gmail.com <szeyic@gmail.com>
2024-02-14 16:06:54 -0500

    class activities

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+// import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import Form from './components/snapshot2'
+import Counter from './components/queueUpdates'
+import RequestTracker from './qcomps/shoppingCart'
+import Scoreboard from './qcomps/updObjectsForm'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Scoreboard />
     </div>
   )
 }
